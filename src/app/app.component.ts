581448e98ff85fa0600aec2e7097bf761df284cc
Vivekanand P V <vivekanandpv@outlook.com>
2020-04-07 22:00:26 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-directives';
+  cssString = 'font-weight-bold';
+  cssStringArray = ['text-success', 'font-italic'];
+  cssObject = {
+    'text-danger': true,
+    'font-weight-bold': true,
+  };
 }
